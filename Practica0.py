5ee25f572a9feb30d767f7a9afa7b09eb38f2ee9
Heigz <UchihaItachi@ciencias.unam.mx>
2024-01-31 22:57:49 -0600

    add: Solucion.txt, modified:Practica0.py

--- Practica0.py
+++ Practica0.py
@@ -3,19 +3,22 @@ from Crypto.Util.Padding import pad, unpad
 from Crypto.Cipher import AES
 from hashlib import sha256
 
-plainkey='RGV2ZWxvcGVyQ29kZTIhQA=='
-iv = b'8\xc0-\x93\x0f\nR^\x973\x1a\xeb]\xeb\xae\x89'
+plainkey = "RGV2ZWxvcGVyQ29kZTIhQA=="
+iv = b"8\xc0-\x93\x0f\nR^\x973\x1a\xeb]\xeb\xae\x89"
+
 
 def cipher(data):
-    data_hex = data.encode('utf-8')
-    key = b64encode(plainkey.encode('utf-8'))
+    data_hex = data.encode("utf-8")
+    key = b64encode(plainkey.encode("utf-8"))
     aes = AES.new(key, AES.MODE_CBC, iv)
     return aes.encrypt(pad(data_hex, 16))
 
+
 def decipher(data):
-    key = b64encode(plainkey.encode('utf-8'))
+    key = b64encode(plainkey.encode("utf-8"))
     aes = AES.new(key, AES.MODE_CBC, iv)
     return unpad(aes.decrypt(data), 16)
+
 
 def validate(passwd, hash):
     ct = cipher(passwd)
@@ -24,6 +27,7 @@ def validate(passwd, hash):
     m = m.hexdigest()
     return m == hash
 
-if __name__ == '__main__':
-    name = 'Fernando Fong' #Inserte su nombre aquí.
+
+if __name__ == "__main__":
+    name = "Gael Alejandro Aguirre Morales"  # Inserte su nombre aquí.
     print(cipher(name))
