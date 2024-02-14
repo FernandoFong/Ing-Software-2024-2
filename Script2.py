898bb1fa6647e0937f6aefb4bee89093b6336234
IsaacAE <alcantara_estrada_kevin@ciencias.unam.mx>
2024-02-14 14:22:21 -0600

    Agregando la parte del arbol binario al Script2

--- Script2.py
+++ Script2.py
@@ -17,13 +17,63 @@ def contar_pasos_negativos(cadena):
                 contador += 1
 
         return contador
-        
+
     except ValueError as e:
         print(f"Error: {e}")
-if __name__ == "__main__":
-    # Obtén la cadena de la terminal
-    cadena_pasos = input("Ingresa la cadena de pasos (por ejemplo, DUDUDUDU): ")
 
-    # Llama a la función y muestra el resultado
-    resultado = contar_pasos_negativos(cadena_pasos)
-    print(f"El número de valles es: {resultado}")
+class Nodo:
+    def __init__(self, valor):
+        self.valor = valor
+        self.izquierda = None
+        self.derecha = None
+
+class Arbol:
+    # Funciones privadas
+    def __init__(self, valor):
+        self.raiz = Nodo(valor)
+
+    def agregar_nodo(self, nodo, valor):
+        if valor < nodo.valor:
+            if nodo.izquierda is None:
+                nodo.izquierda = Nodo(valor)
+            else:
+                self.agregar_nodo(nodo.izquierda, valor)
+        else:
+            if nodo.derecha is None:
+                nodo.derecha = Nodo(valor)
+            else:
+                self.agregar_nodo(nodo.derecha, valor)
+
+    def recorrido_in_orden(self, nodo):
+        if nodo is not None:
+            self.recorrido_in_orden(nodo.izquierda)
+            print(nodo.valor, end=" ")
+            self.recorrido_in_orden(nodo.derecha)
+
+    def recorrido_pre_orden(self, nodo):
+        if nodo is not None:
+            print(nodo.valor, end=" ")
+            self.recorrido_pre_orden(nodo.izquierda)
+            self.recorrido_pre_orden(nodo.derecha)
+
+    def recorrido_post_orden(self, nodo):
+        if nodo is not None:
+            self.recorrido_post_orden(nodo.izquierda)
+            self.recorrido_post_orden(nodo.derecha)
+            print(nodo.valor, end=" ")
+
+    # estas son las funciones que se deben llamar para agregar y obtener recorridos.
+
+    def agregar(self, valor):
+        self.agregar_nodo(self.raiz, valor)
+   
+    def pre_orden(self):
+        self.recorrido_pre_orden(self.raiz)
+
+    def in_orden(self):
+        self.recorrido_in_orden(self.raiz)
+
+    def post_orden(self):
+        self.recorrido_post_orden(self.raiz)
+
+
