b6d89aa35ae961722e66122c1f81e5115eb2a068
PaolaResendiz <92765346+PaolaResendiz@users.noreply.github.com>
2024-02-16 00:36:58 -0600

    agregando archivos

--- __init__.py
+++ __init__.py
@@ -1,8 +1,42 @@
-import logging
-from fontTools.misc.loggingTools import configLogger
+# --------------------------------------------------------------------------------------
+# Copyright (c) 2013-2022, Nucleic Development Team.
+#
+# Distributed under the terms of the Modified BSD License.
+#
+# The full license is in the file LICENSE, distributed with this software.
+# --------------------------------------------------------------------------------------
+from ._cext import (
+    Constraint,
+    Expression,
+    Solver,
+    Term,
+    Variable,
+    __kiwi_version__,
+    __version__,
+    strength,
+)
+from .exceptions import (
+    BadRequiredStrength,
+    DuplicateConstraint,
+    DuplicateEditVariable,
+    UnknownConstraint,
+    UnknownEditVariable,
+    UnsatisfiableConstraint,
+)
 
-log = logging.getLogger(__name__)
-
-version = __version__ = "4.49.0"
-
-__all__ = ["version", "log", "configLogger"]
+__all__ = [
+    "BadRequiredStrength",
+    "DuplicateConstraint",
+    "DuplicateEditVariable",
+    "UnknownConstraint",
+    "UnknownEditVariable",
+    "UnsatisfiableConstraint",
+    "strength",
+    "Variable",
+    "Term",
+    "Expression",
+    "Constraint",
+    "Solver",
+    "__version__",
+    "__kiwi_version__",
+]
